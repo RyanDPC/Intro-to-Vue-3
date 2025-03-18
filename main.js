b8e2cddc94a7ef1dabd5b44814f1a7ff63e4b215
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-03-18 14:07:17 +0100

    feat: we can view Socks and T-shirt on the page

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const product = ["Socks", "Boots"];
+const app = Vue.createApp({
+  data() {
+    return { product: product };
+  },
+});
+const testing = Vue.createApp({
+  data() {
+    return { test: (test = ["T-shirt", "Pullover"]) };
+  },
+});
