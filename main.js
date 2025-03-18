41123871d91ca80113cd500fa838c24d6943680a
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-03-18 14:30:02 +0100

    feat: L3 start

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const product = ["Socks", "Boots"];
+const productImage = [
+  "./assets/images/socks_blue.jpg",
+  "./assets/images/socks_green.jpg",
+];
+const description = [
+  "Une paire de chaussettes chaudes et confortables",
+  "Un t-shirt en coton",
+];
 const app = Vue.createApp({
   data() {
-    return { product: product };
+    return {
+      product: product[0],
+      productImage: productImage[0],
+      description: description[0],
+    };
   },
 });
+const test = ["T-shirt", "Pullover"];
 const testing = Vue.createApp({
   data() {
-    return { test: (test = ["T-shirt", "Pullover"]) };
+    return {
+      test: test[0],
+      description: description[1],
+    };
   },
 });
