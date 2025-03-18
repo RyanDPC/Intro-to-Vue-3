1b67fb2a4579bd17420f32818cae523f6d422cc5
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-03-18 15:43:08 +0100

    feat: add key attributs for optimize the code

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const products_descriptions = [
   "Une paire de chaussettes chaudes et confortables",
   "Une paire de chaussure haute",
 ];
+const products_size = [
+  { id: 1, size: "S" },
+  { id: 2, size: "M" },
+  { id: 3, size: "L" },
+  { id: 4, size: "XXL" },
+];
 const header = Vue.createApp({});
 const nav = Vue.createApp({});
 const app = Vue.createApp({
@@ -16,6 +22,7 @@ const app = Vue.createApp({
       products: products[0],
       products_images: products_images[0],
       products_descriptions: products_descriptions[0],
+      products_size: products_size,
       inventory: 1,
     };
   },
