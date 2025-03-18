ba62aebb0b50388b6adffd1526295337887a0f0d
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-03-18 17:20:07 +0100

    feat: L8 end

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const variants = [
     description: "Une paire de chaussettes chaudes et confortables",
     inventory: 10,
     color: "blue",
+    inStock: true,
     sizes: [{ name: "S" }, { name: "M" }, { name: "L" }, { name: "XXL" }],
+    quantity: 50,
   },
   {
     id: 2,
@@ -15,34 +17,50 @@ const variants = [
     description: "Une paire de chaussure haute",
     inventory: 0,
     color: "green",
+    inStock: false,
     sizes: [{ name: "S" }, { name: "M" }, { name: "L" }, { name: "XXL" }],
+    quantitty: 0,
   },
 ];
 
 const app = Vue.createApp({
   data() {
     return {
+      brand: "Vue Mastery",
       cart: 0,
       variants: variants,
-      currentVariant: variants[0],
+      selectedVariant: 0,
       url: "https://www.etml.ch/",
+      onSale: true,
     };
   },
   methods: {
     addToCart() {
       this.cart += 1;
     },
-    updateImage(variantImage) {
-      this.currentVariant.image = variantImage;
-    },
-    updateVariant(color) {
-      this.currentVariant = this.variants.find(
-        (variant) => variant.color === color
-      );
+    updateVariant(index) {
+      this.selectedVariant = index;
     },
     remove() {
       if (this.cart > 0) this.cart -= 1;
       else return;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    variant() {
+      return this.variants[this.selectedVariant];
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      if (this.onSale) this.brand + " " + this.product + " are on sale!";
     },
   },
 });
